0cccc0b85d14a797d9bf3e72946bb1536aadd410
Visual Ehrmanntraut <30368284+VisualEhrmanntraut@users.noreply.github.com>
2025-08-22 11:31:34 +0300

    refactor(xnu_kpf): general cleanup

--- hw/arm/apple-silicon/xnu_kpf.c
+++ hw/arm/apple-silicon/xnu_kpf.c
@@ -13,9 +13,6 @@
 static uint32_t *find_next_insn(uint32_t *buffer, uint32_t num, uint32_t insn,
                                 uint32_t mask)
 {
-    g_assert_nonnull(buffer);
-    g_assert_cmpuint(num, !=, 0);
-    g_assert_cmphex(mask, !=, 0);
     g_assert_cmphex(insn & mask, ==, insn);
 
     for (uint32_t i = 0; i < num; ++i) {
@@ -32,9 +29,6 @@ static uint32_t *find_next_insn(uint32_t *buffer, uint32_t num, uint32_t insn,
 static uint32_t *find_prev_insn(uint32_t *buffer, uint32_t num, uint32_t insn,
                                 uint32_t mask)
 {
-    g_assert_nonnull(buffer);
-    g_assert_cmpuint(num, !=, 0);
-    g_assert_cmphex(mask, !=, 0);
     g_assert_cmphex(insn & mask, ==, insn);
 
     for (uint32_t i = 0; i < num; ++i) {
@@ -52,14 +46,15 @@ static bool kpf_apfs_rootauth(ApplePfPatch *patch, uint32_t *opcode_stream)
     opcode_stream[0] = NOP;
     opcode_stream[1] = 0x52800000; // mov w0, 0
 
-    puts("KPF: found handle_eval_rootauth");
+    info_report("%s: Found handle_eval_rootauth", __func__);
     return true;
 }
 
 static bool kpf_apfs_vfsop_mount(ApplePfPatch *patch, uint32_t *opcode_stream)
 {
     opcode_stream[0] = 0x52800000; // mov w0, 0
-    puts("KPF: found apfs_vfsop_mount");
+
+    info_report("%s: Found apfs_vfsop_mount", __func__);
     return true;
 }
 
@@ -69,9 +64,12 @@ static void kpf_apfs_patches(ApplePfPatchset *patchset)
     uint64_t matches[] = {
         0x37280068, // tbnz w8, 5, 0xC
         0X52800A00, // mov w0, 0x50
-        0xD65F03C0 // ret
+        0xD65F03C0, // ret
     };
     uint64_t masks[] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
+
+    xnu_pf_maskmatch(patchset, "handle_eval_rootauth", matches, masks,
+                     ARRAY_SIZE(masks), (void *)kpf_apfs_rootauth);
 
     // Allow mounting root as R/W
     uint64_t matches2[] = {
@@ -88,13 +86,8 @@ static void kpf_apfs_patches(ApplePfPatchset *patchset)
         0xFFC003A0,
     };
 
-    xnu_pf_maskmatch(patchset, "handle_eval_rootauth", matches, masks,
-                     sizeof(masks) / sizeof(uint64_t),
-                     (void *)kpf_apfs_rootauth);
-
     xnu_pf_maskmatch(patchset, "apfs_vfsop_mount", matches2, masks2,
-                     sizeof(masks2) / sizeof(uint64_t),
-                     (void *)kpf_apfs_vfsop_mount);
+                     ARRAY_SIZE(masks2), (void *)kpf_apfs_vfsop_mount);
 }
 
 static bool kpf_amfi_callback(ApplePfPatch *patch, uint32_t *opcode_stream)
@@ -103,6 +96,7 @@ static bool kpf_amfi_callback(ApplePfPatch *patch, uint32_t *opcode_stream)
         ((opcode_stream[-2] & 0xFF000000) != 0x91000000)) {
         return false;
     }
+
     uint32_t *ldrb = find_next_insn(opcode_stream, 256, 0x39402C00, 0xFFFFFC00);
     uint32_t cdhash_param = extract32(*ldrb, 5, 5);
     uint32_t *frame;
@@ -111,16 +105,16 @@ static bool kpf_amfi_callback(ApplePfPatch *patch, uint32_t *opcode_stream)
 
     frame = find_prev_insn(opcode_stream, 10, 0x910003FD, 0xFF8003FF);
     if (frame == NULL) {
-        info_report("Found AMFI (Leaf)");
+        info_report("%s: Found AMFI (Leaf)", __func__);
     } else {
-        info_report("Found AMFI (Routine)");
+        info_report("%s: Found AMFI (Routine)", __func__);
         start = find_prev_insn(frame, 10, 0xA9A003E0, 0xFFE003E0);
         if (start == NULL) {
             start = find_prev_insn(frame, 10, 0xD10003FF, 0xFF8003FF);
-        }
-        if (start == NULL) {
-            info_report("Failed to find AMFI start");
-            return false;
+            if (start == NULL) {
+                error_report("%s: Failed to find AMFI start", __func__);
+                return false;
+            }
         }
     }
 
@@ -132,21 +126,21 @@ static bool kpf_amfi_callback(ApplePfPatch *patch, uint32_t *opcode_stream)
         if (adrp != NULL) {
             start = adrp;
         }
-        info_report("lookup_in_static_trust_cache @ 0x%" PRIx64,
+        info_report("%s: lookup_in_static_trust_cache @ 0x%" PRIx64, __func__,
                     ptov_static((hwaddr)start));
-        *(start++) = 0x52802020; // MOV W0, 0x101
-        *(start++) = (pac ? RETAB : RET);
+        *start++ = 0x52802020; // MOV W0, 0x101
+        *start++ = (pac ? RETAB : RET);
         return true;
     }
     case 1:
-        info_report("lookup_in_trust_cache_module @ 0x%" PRIx64,
+        info_report("%s: lookup_in_trust_cache_module @ 0x%" PRIx64, __func__,
                     ptov_static((hwaddr)start));
-        *(start++) = 0x52800040; // mov w0, 2
-        *(start++) = 0x39000040; // strb w0, [x2]
-        *(start++) = 0x52800020; // mov w0, 1
-        *(start++) = 0x39000060; // strb w0, [x3]
-        *(start++) = 0x52800020; // mov w0, 1
-        *(start++) = (pac ? RETAB : RET);
+        *start++ = 0x52800040; // mov w0, 2
+        *start++ = 0x39000040; // strb w0, [x2]
+        *start++ = 0x52800020; // mov w0, 1
+        *start++ = 0x39000060; // strb w0, [x3]
+        *start++ = 0x52800020; // mov w0, 1
+        *start++ = (pac ? RETAB : RET);
         return true;
     default:
         error_report("Found unexpected AMFI prototype: %d", cdhash_param);
@@ -162,12 +156,11 @@ static void kpf_amfi_patch(ApplePfPatchset *patchset)
     uint64_t matches[] = {
         0x52800200, // mov w*, 0x16
         0xD3000000, // lsr *
-        0x9B000000 // madd *
+        0x9B000000, // madd *
     };
     uint64_t masks[] = { 0xFFFFFF00, 0xFF000000, 0xFF000000 };
     xnu_pf_maskmatch(patchset, "amfi_patch", matches, masks,
-                     sizeof(matches) / sizeof(uint64_t),
-                     (void *)kpf_amfi_callback);
+                     ARRAY_SIZE(matches), (void *)kpf_amfi_callback);
 }
 
 static bool kpf_trustcache_callback(ApplePfPatch *patch,
@@ -178,35 +171,34 @@ static bool kpf_trustcache_callback(ApplePfPatch *patch,
     if (start == NULL) {
         return false;
     }
-    info_report("pmap_lookup_in_static_trust_cache_internal @ 0x%" PRIx64,
-                ptov_static((hwaddr)start));
-    *(start++) = 0x52802020; // mov w0, 0x101
-    *(start++) = RET;
+
+    info_report("%s: pmap_lookup_in_static_trust_cache_internal @ 0x%" PRIx64,
+                __func__, ptov_static((hwaddr)start));
+    *start++ = 0x52802020; // mov w0, 0x101
+    *start++ = RET;
+
     return true;
 }
 
 static void kpf_trustcache_patch(ApplePfPatchset *patchset)
 {
-    uint64_t matches[] = {
-        0xD29DCFC0, // mov w*, 0xEE7E
-    };
-    uint64_t masks[] = {
-        0xFFFFFFC0,
-    };
+    uint64_t matches[] = { 0xD29DCFC0 }; // mov w*, 0xEE7E
+    uint64_t masks[] = { 0xFFFFFFC0 };
     xnu_pf_maskmatch(patchset, "trustcache16", matches, masks,
-                     sizeof(matches) / sizeof(uint64_t),
-                     (void *)kpf_trustcache_callback);
+                     ARRAY_SIZE(matches), (void *)kpf_trustcache_callback);
 }
 
 static bool kpf_amfi_sha1(ApplePfPatch *patch, uint32_t *opcode_stream)
 {
     uint32_t *cmp = find_next_insn(opcode_stream, 0x10, 0x7100081F,
                                    0xFFFFFFFF); // cmp w0, 2
-    if (!cmp) {
-        puts("kpf_amfi_sha1: failed to find cmp");
+
+    if (cmp == NULL) {
+        error_report("%s: failed to find cmp", __func__);
         return false;
     }
-    puts("KPF: Found AMFI hashtype check");
+
+    info_report("Found AMFI hashtype check");
     xnu_pf_disable_patch(patch);
     *cmp = 0x6B00001F; // cmp w0, w0
     return true;
@@ -215,30 +207,24 @@ static bool kpf_amfi_sha1(ApplePfPatch *patch, uint32_t *opcode_stream)
 static void kpf_amfi_kext_patches(ApplePfPatchset *patchset)
 {
     // Allow running binaries with SHA1 signatures
-    uint64_t i_matches[] = {
-        0x36D00002, // tbz w2, 0x1A, *
-    };
-    uint64_t i_masks[] = {
-        0xFFF8001F,
-    };
+    uint64_t i_matches[] = { 0x36D00002 }; // tbz w2, 0x1A, *
+    uint64_t i_masks[] = { 0xFFF8001F };
     xnu_pf_maskmatch(patchset, "amfi_sha1", i_matches, i_masks,
-                     sizeof(i_matches) / sizeof(uint64_t),
-                     (void *)kpf_amfi_sha1);
+                     ARRAY_SIZE(i_matches), (void *)kpf_amfi_sha1);
 }
 
 static bool kpf_mac_mount_callback(ApplePfPatch *patch, uint32_t *opcode_stream)
 {
-    info_report("Found mac_mount");
     uint32_t *mac_mount = &opcode_stream[0];
+
     uint32_t *mac_mount_1 =
         find_prev_insn(mac_mount, 0x40, 0x37280000, 0xFFFE0000);
-
     if (mac_mount_1 == NULL) {
         mac_mount_1 = find_next_insn(mac_mount, 0x40, 0x37280000, 0xFFFE0000);
-    }
-    if (mac_mount_1 == NULL) {
-        puts("kpf_mac_mount_callback: failed to find NOP point");
-        return false;
+        if (mac_mount_1 == NULL) {
+            error_report("%s: failed to find NOP point", __func__);
+            return false;
+        }
     }
 
     // Allow MNT_UNION mounts
@@ -246,40 +232,34 @@ static bool kpf_mac_mount_callback(ApplePfPatch *patch, uint32_t *opcode_stream)
 
     // Search for ldrb w8, [x*, 0x71]
     mac_mount_1 = find_prev_insn(mac_mount, 0x40, 0x3941C408, 0xFFFFFC1F);
-    if (!mac_mount_1) {
+    if (mac_mount_1 == NULL) {
         mac_mount_1 = find_next_insn(mac_mount, 0x40, 0x3941C408, 0xFFFFFC1F);
-    }
-    if (!mac_mount_1) {
-        puts("kpf_mac_mount_callback: failed to find xzr point");
-        return false;
+        if (mac_mount_1 == NULL) {
+            error_report("%s: failed to find xzr point", __func__);
+            return false;
+        }
     }
 
     // Replace with a mov x8, xzr
     // This will bypass the (vp->v_mount->mnt_flag & MNT_ROOTFS) check
     mac_mount_1[0] = 0xAA1F03E8;
+
     xnu_pf_disable_patch(patch);
 
-    puts("KPF: Found mac_mount");
+    info_report("Found mac_mount");
     return true;
 }
 
 static void kpf_mac_mount_patch(ApplePfPatchset *patchset)
 {
     // This patch will allow us to remount the rootfs and do UNION mounts.
-    uint64_t matches[] = {
-        0x321F2FE9, // orr w9, wzr, 0x1FFE
-    };
-    uint64_t masks[] = {
-        0xFFFFFFFF,
-    };
-
+    uint64_t matches[] = { 0x321F2FE9 }; // orr w9, wzr, 0x1FFE
+    uint64_t masks[] = { 0xFFFFFFFF };
     xnu_pf_maskmatch(patchset, "mac_mount_patch1", matches, masks,
-                     sizeof(matches) / sizeof(uint64_t),
-                     (void *)kpf_mac_mount_callback);
+                     ARRAY_SIZE(matches), (void *)kpf_mac_mount_callback);
     matches[0] = 0x5283FFC9; // movz w9, 0x1FFE
     xnu_pf_maskmatch(patchset, "mac_mount_patch2", matches, masks,
-                     sizeof(matches) / sizeof(uint64_t),
-                     (void *)kpf_mac_mount_callback);
+                     ARRAY_SIZE(matches), (void *)kpf_mac_mount_callback);
 }
 
 void xnu_kpf(MachoHeader64 *hdr)
@@ -324,10 +304,10 @@ void xnu_kpf(MachoHeader64 *hdr)
 
     kpf_amfi_patch(ppltext_patchset);
     kpf_trustcache_patch(ppltext_patchset);
-    if (ppltext_exec) {
-        xnu_pf_apply(ppltext_exec, ppltext_patchset);
-    } else {
+    if (ppltext_exec == NULL) {
         warn_report("Failed to find `__PPLTEXT`.");
+    } else {
+        xnu_pf_apply(ppltext_exec, ppltext_patchset);
     }
     xnu_pf_patchset_destroy(ppltext_patchset);
 
